e136b795272168f8f28c5163dd2164e0749b2f79
Fauziyyan Rahman <37616113+FauziyyanRahman@users.noreply.github.com>
2023-09-06 10:00:22 +0700

    adding cache

---
+++ storage/framework/views/63c10ada6aa11b4ef2901d2fd7f5a95c.php
@@ -0,0 +1,72 @@
+<header id="page-topbar">
+    <div class="navbar-header">
+        <div class="d-flex">
+            <!-- LOGO -->
+            <div class="navbar-brand-box">
+                <a href="index" class="logo logo-dark">                    
+                    <span class="logo-lg">
+                        <img src="<?php echo e(URL::asset('build/images/hino-logo.png')); ?>" alt="" height="45">
+                    </span>
+                </a>
+
+                <a href="index" class="logo logo-light">
+                    <span class="logo-lg">
+                        <img src="<?php echo e(URL::asset('build/images/hino-logo.png')); ?>" alt="" height="45">
+                    </span>
+                </a>
+            </div>
+
+            <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
+                <i class="fa fa-fw fa-bars"></i>
+            </button>
+        </div>
+
+        <div class="d-flex">
+
+            <div class="dropdown d-inline-block d-lg-none ms-2">
+                <button type="button" class="btn header-item noti-icon waves-effect"
+                    id="page-header-search-dropdown" data-bs-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <i class="mdi mdi-magnify"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
+                    aria-labelledby="page-header-search-dropdown">
+
+                    <form class="p-3">
+                        <div class="form-group m-0">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="<?php echo app('translator')->get('translation.Search'); ?>"
+                                    aria-label="Search input">
+
+                                <button class="btn btn-primary" type="submit"><i
+                                        class="mdi mdi-magnify"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="dropdown d-inline-block">
+                <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
+                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img class="rounded-circle header-profile-user"
+                        src="<?php echo e(isset(Auth::user()->avatar) ? asset(Auth::user()->avatar) : asset('build/images/user-dummy-img.jpg')); ?>"
+                        alt="Header Avatar">
+                    <span class="d-none d-xl-inline-block ms-1" key="t-henry"><?php echo e(ucfirst(session('Username'))); ?></span>
+                    <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-end">
+                    <!-- item-->
+                    <a class="dropdown-item" href="https://karoseri.hino.co.id/home"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Karoseri Catalog</span></a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item text-danger" href="javascript:void();"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
+                            class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span
+                            key="t-logout"><?php echo app('translator')->get('translation.Logout'); ?></span></a>
+                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                        <?php echo csrf_field(); ?>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</header><?php /**PATH /Volumes/Secondary/PHP/Laravel/Apps/Apps-Hino/resources/views/layouts/topbar.blade.php ENDPATH**/ ?>
